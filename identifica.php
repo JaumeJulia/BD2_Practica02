334c678861fcc1aa1110047c4a679c8356d3e06c
Pau Capellá Ballester <pau.capella@hotmail.es>
2021-12-10 19:19:13 +0100

    Algunas modificaciones

--- identifica.php
+++ identifica.php
@@ -1,7 +1,5 @@
 <?php session_start();
 
-
-
 while (!isset($_SESSION)) {
     
     echo "session no iniciada";
@@ -9,16 +7,20 @@ while (!isset($_SESSION)) {
     $user= $_POST['uname'];
     $pass= $_POST['psw'];
       
-   
 }
 
 include "conexion.php";
+
 $user= $_POST['uname'];
+
 $pass= $_POST['psw'];
+
 $cadena = "select admin from usuari WHERE usuari.nomUsuari='".$user."' AND usuari.contrasenya ='".$pass."'";
+
 $result = mysqli_query($con,$cadena);
 
 if (mysqli_num_rows($result) > 0){
+    
     $row = mysqli_fetch_row($result);
 
     if($row[0]==1){
@@ -33,10 +35,6 @@ if (mysqli_num_rows($result) > 0){
     echo('<span style="color: red;" /><center> Usuario o contraseña incorrectos </center></span>');
 }
 
-
 mysqli_close($con);
 
 ?>
-
-
-
