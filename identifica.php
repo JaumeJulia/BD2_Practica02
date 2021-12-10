7b394a3cefe76a2dc9b0988777b167f490eb2645
Sr_Tortugo98 <carlosecker.sp@gmail.com>
2021-12-10 18:49:28 +0100

    Create identifica.php
    Mira si el usuario y contraseña son correctos y si es admin

---
+++ identifica.php
@@ -0,0 +1,42 @@
+<?php session_start();
+
+
+
+while (!isset($_SESSION)) {
+    
+    echo "session no iniciada";
+    // Recollida de paràmetres
+    $user= $_POST['uname'];
+    $pass= $_POST['psw'];
+      
+   
+}
+
+include "conexion.php";
+$user= $_POST['uname'];
+$pass= $_POST['psw'];
+$cadena = "select admin from usuari WHERE usuari.nomUsuari='".$user."' AND usuari.contrasenya ='".$pass."'";
+$result = mysqli_query($con,$cadena);
+
+if (mysqli_num_rows($result) > 0){
+    $row = mysqli_fetch_row($result);
+
+    if($row[0]==1){
+        echo("Eres ADMIN");
+    }else{
+        echo("Eres no ADMIN");
+        
+    }
+  
+}else{
+    include "index.html";
+    echo('<span style="color: red;" /><center> Usuario o contraseña incorrectos </center></span>');
+}
+
+
+mysqli_close($con);
+
+?>
+
+
+
