a7eef4bf2f86f66a15ce61d895d43ca834dc6d98
Sr_Tortugo98 <carlosecker.sp@gmail.com>
2021-12-19 19:34:22 +0100

    Gestion Admin

---
+++ BD205/PHP/Admin.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<body>
+<style>
+    body {font-family: Arial, Helvetica, sans-serif;}
+    form {border: 3px solid #f1f1f1;}
+    a:link {
+    color: #68b9da;
+    background-color: transparent;
+    text-decoration: none;
+    }
+    a:visited {
+    color: pink;
+    background-color: transparent;
+     text-decoration: none;
+    }
+    a:hover {
+    color: #68b9da;
+    background-color: transparent;
+    text-decoration: underline;
+    }
+    a:active {
+    color: yellow;
+    background-color: transparent;
+    text-decoration: underline;
+    }
+        button {
+        color: white;
+        padding: 14px 20px;
+        margin: 8px 0;
+        border:none;
+        border-radius: 8px;
+        cursor: pointer;
+        width: 20%;
+        }
+        .button1 {background-color: white  ;border: 2px solid #68b9da;}
+        .button2 {background-color: white  ;border: 2px solid pink;}
+    </style>
+<center><img src="../Images/Notflix.PNG" width="300"></center>
+<br>
+<table align="center">
+    <tr><td><a class="button button1" href="Usuario.php">Añadir Usuario</a></td></tr>
+    <tr><td><a class="button button2" href="Usuario.php">Añadir Contenido</a></td></tr>
+    <tr><td><a class="button button1" href="Usuario.php">Modificar Tabla Usuarios</a></td></tr>
+    <tr><td><a class="button button2" href="Usuario.php">Modificar Tabla Contenidos</a></td></tr>
+</table>
+</body>
+</html>
