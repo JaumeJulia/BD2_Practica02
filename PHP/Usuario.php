7b6b40b1968f1a3b25384cc63cf468da0ab30926
Alberto-Cugat <67785595+Alberto-Cugat@users.noreply.github.com>
2021-12-19 19:21:03 +0100

    Mi carpetita

--- PHP/Usuario.php
+++ PHP/Usuario.php
@@ -35,6 +35,7 @@ a:active {
         }
         .button1 {background-color: white  ;border: 2px solid #68b9da;}
         .button2 {background-color: white  ;border: 2px solid pink;}
+        .button3 {background-color: white  ;border: 2px solid red;}
     </style>
 <center><img src="../Images/Notflix.PNG" width="300"></center>
 <br>
@@ -45,12 +46,13 @@ if(!array_key_exists('uname', $_POST)){
 }else{
   $usuario = $_POST['uname'];
 }
-$consulta_r = '<a href="ConsultarRecomanats.php?uname='.$usuario.'" class="button button1"style="font-size:20px" >Consultar Recomendados</a>';
-$visualizar_t = '<a href="Visualizartabla.php?uname='.$usuario.'" class="button button2"style="font-size:20px" >Visualizar Facturas</a>';
+$consulta_recomendados = '<a href="ConsultarRecomanats.php?uname='.$usuario.'" class="button button1"style="font-size:20px" >Consultar Recomendados</a>';
+$visualizar_facturas = '<a href="Visualizartabla.php?uname='.$usuario.'" class="button button2"style="font-size:20px" >Visualizar Facturas</a>';
+$consultar_contenidos = '<a href="ConsultarContenido.php?uname='.$usuario.'" class="button button3"style="font-size:20px" >Ver Contenidos</a>';
 echo "<table align='center'>";
-    echo "<tr><td>".$consulta_r. "</td></tr>";
-    echo "<tr><td>".$visualizar_t."</td></tr>";
-
+    echo "<tr><td>".$consulta_recomendados. "</td></tr>";
+    echo "<tr><td>".$visualizar_facturas."</td></tr>";
+    echo "<tr><td>".$consultar_contenidos."</td></tr>";
 echo "</table>";
 ?>
 </body>
