bae8dfe20b43beb155392290a45f4abb40dff015
Alberto-Cugat <67785595+Alberto-Cugat@users.noreply.github.com>
2021-12-21 00:30:59 +0100

    Update ConsultarContenidoFavorito.php

--- BD205/BD243466710E/ConsultarContenidoFavorito.php
+++ BD205/BD243466710E/ConsultarContenidoFavorito.php
@@ -35,71 +35,71 @@
         $sql_edad="SELECT usuari.tipusUsuari FROM usuari WHERE usuari.nomUsuari='".$user."'";
         $edad=mysqli_query($con,$sql_edad);
         if(mysqli_num_rows($edad) > 0){
-        $edad=mysqli_fetch_array($edad);
-        switch($edad['tipusUsuari']){
-            case ">18":
-                $sql_18="SELECT contingut.titol,contingut.categoria,contingut.video FROM contracte  
-                            INNER JOIN (contfav
-                                INNER JOIN (contingut
-                                    INNER JOIN recomanat
-                                    ON contingut.video=recomanat.video AND recomanat.tipusUsuari='>18')
-                                ON contfav.video=contingut.video)
-                            ON contracte.idContracte=contfav.idContracte AND contracte.suscrit=1 AND contracte.nomUsuari='".$user."'";
-                
-                $result=mysqli_query($con,$sql_18);
-                if(mysqli_num_rows($result) > 0){
-                    while($mostrar=mysqli_fetch_array($result)){
-        ?>
-                        <tr>
-                            <td><center><?php echo $mostrar['titol'] ?></center></td> 
-                            <td><center><?php echo $mostrar['categoria'] ?></center></td> 
-                            <td><center><?php echo $mostrar['video'] ?></center></td> 
-                        </tr>
-                        <?php
-                    }
-                }
-            case "9-18":
-                $sql_9_18="SELECT contingut.titol,contingut.categoria,contingut.video FROM contracte  
-                                INNER JOIN (contfav
-                                    INNER JOIN (contingut
-                                        INNER JOIN recomanat
-                                        ON contingut.video=recomanat.video AND recomanat.tipusUsuari='9-18')
-                                    ON contfav.video=contingut.video)
-                                ON contracte.idContracte=contfav.idContracte AND contracte.suscrit=1 AND contracte.nomUsuari='".$user."'";
+            $edad=mysqli_fetch_array($edad);
+                switch($edad['tipusUsuari']){
+                    case ">18":
+                        $sql_18="SELECT contingut.titol,contingut.categoria,contingut.video FROM contracte  
+                                    INNER JOIN (contfav
+                                        INNER JOIN (contingut
+                                            INNER JOIN recomanat
+                                            ON contingut.video=recomanat.video AND recomanat.tipusUsuari='>18')
+                                        ON contfav.video=contingut.video)
+                                    ON contracte.idContracte=contfav.idContracte AND contracte.suscrit=1 AND contracte.nomUsuari='".$user."'";
 
-                $result=mysqli_query($con,$sql_9_18);
-                if(mysqli_num_rows($result) > 0){
-                    while($mostrar=mysqli_fetch_array($result)){
-        ?>
-                            <tr>
-                                <td><center><?php echo $mostrar['titol'] ?></center></td> 
-                                <td><center><?php echo $mostrar['categoria'] ?></center></td> 
-                                <td><center><?php echo $mostrar['video'] ?></center></td> 
-                            </tr>
-                            <?php
-                    }
-                }
-            case "<9":
-                $sql_9="SELECT contingut.titol,contingut.categoria,contingut.video FROM contracte  
-                            INNER JOIN (contfav
-                                INNER JOIN (contingut
-                                    INNER JOIN recomanat
-                                    ON contingut.video=recomanat.video AND recomanat.tipusUsuari='<9')
-                                ON contfav.video=contingut.video)
-                            ON contracte.idContracte=contfav.idContracte AND contracte.suscrit=1 AND contracte.nomUsuari='".$user."'";
-
-                $result=mysqli_query($con,$sql_9);
-                if(mysqli_num_rows($result) > 0){
-                    while($mostrar=mysqli_fetch_array($result)){
-        ?>
-                        <tr>
-                            <td><center><?php echo $mostrar['titol'] ?></center></td> 
-                            <td><center><?php echo $mostrar['categoria'] ?></center></td> 
-                            <td><center><?php echo $mostrar['video'] ?></center></td> 
-                        </tr>
-                        <?php
-                    }
-                }
+                        $result=mysqli_query($con,$sql_18);
+                        if(mysqli_num_rows($result) > 0){
+                            while($mostrar=mysqli_fetch_array($result)){
+        ?>      
+                                <tr>
+                                    <td><center><?php echo $mostrar['titol'] ?></center></td> 
+                                    <td><center><?php echo $mostrar['categoria'] ?></center></td> 
+                                    <td><center><?php echo $mostrar['video'] ?></center></td> 
+                                </tr>
+                                <?php
+                            }
+                        }
+                    case "9-18":
+                        $sql_9_18="SELECT contingut.titol,contingut.categoria,contingut.video FROM contracte  
+                                        INNER JOIN (contfav
+                                            INNER JOIN (contingut
+                                                INNER JOIN recomanat
+                                                ON contingut.video=recomanat.video AND recomanat.tipusUsuari='9-18')
+                                            ON contfav.video=contingut.video)
+                                        ON contracte.idContracte=contfav.idContracte AND contracte.suscrit=1 AND contracte.nomUsuari='".$user."'";
+        
+                        $result=mysqli_query($con,$sql_9_18);
+                        if(mysqli_num_rows($result) > 0){
+                            while($mostrar=mysqli_fetch_array($result)){
+        ?>      
+                                    <tr>
+                                        <td><center><?php echo $mostrar['titol'] ?></center></td> 
+                                        <td><center><?php echo $mostrar['categoria'] ?></center></td> 
+                                        <td><center><?php echo $mostrar['video'] ?></center></td> 
+                                    </tr>
+                                    <?php
+                            }
+                        }
+                    case "<9":
+                        $sql_9="SELECT contingut.titol,contingut.categoria,contingut.video FROM contracte  
+                                    INNER JOIN (contfav
+                                        INNER JOIN (contingut
+                                            INNER JOIN recomanat
+                                            ON contingut.video=recomanat.video AND recomanat.tipusUsuari='<9')
+                                        ON contfav.video=contingut.video)
+                                    ON contracte.idContracte=contfav.idContracte AND contracte.suscrit=1 AND contracte.nomUsuari='".$user."'";
+        
+                        $result=mysqli_query($con,$sql_9);
+                        if(mysqli_num_rows($result) > 0){
+                            while($mostrar=mysqli_fetch_array($result)){
+        ?>      
+                                <tr>
+                                    <td><center><?php echo $mostrar['titol'] ?></center></td> 
+                                    <td><center><?php echo $mostrar['categoria'] ?></center></td> 
+                                    <td><center><?php echo $mostrar['video'] ?></center></td> 
+                                </tr>
+                                <?php
+                            }
+                        }
                     break;
         }
     }
