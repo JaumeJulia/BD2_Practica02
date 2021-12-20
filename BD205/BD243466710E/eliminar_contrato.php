b7b0c0116a8801d298445a61da9d9b4eeceef852
Alberto-Cugat <67785595+Alberto-Cugat@users.noreply.github.com>
2021-12-20 20:21:34 +0100

    Update eliminar_contrato.php

--- BD205/BD243466710E/eliminar_contrato.php
+++ BD205/BD243466710E/eliminar_contrato.php
@@ -5,22 +5,34 @@
     <?php
         include "../../PHP/conexion.php";
         $idContracte = $_POST['idContracte'];
+
         $consultaFacturas = 'SELECT idFactura FROM factura WHERE factura.idContracte = "'.$idContracte.'"'; //sacamos las facturas asociadas al contrato
         $facturas = mysqli_query($con, $consultaFacturas);
 
         while($mostrar=mysqli_fetch_array($facturas)){
             $eliminarFactura = 'DELETE FROM factura WHERE factura.idFactura = "'.$mostrar['idFactura'].'"';
+            $resultado = mysqli_query($con,$consulta);
         }
 
-        $arrayContracte = mysqli_fetch_array($contestacioContracte);
-        $idContracte = $arrayContracte['idContracte'];
+        $consultaCatfav = 'SELECT * FROM catfav WHERE catfav.idContracte = "'.$idContracte.'"'; //sacamos las categorias favoritas asociadas al contrato
+        $catfav = mysqli_query($con, $consultaCatfav);
 
-        $consulta = 'DELETE from CATFAV where CATFAV.idContracte = "'.$idContracte.'" AND CATFAV.categoria = "'.$eliminar.'"';
+        while($mostrar=mysqli_fetch_array($catfav)){
+            $eliminarCatfav = 'DELETE FROM catfav WHERE catfav.idContracte = "'.$mostrar['idContracte'].'"';
+            $resultado = mysqli_query($con,$consulta);
+        }
+
+        $consultaContfav = 'SELECT * FROM contfav WHERE contfav.idContracte = "'.$idContracte.'"'; //sacamos los contenidos favoritos asociados al contrato
+        $contfav = mysqli_query($con, $consultaContfav);
+
+        while($mostrar=mysqli_fetch_array($contfav)){
+            $eliminarContfav = 'DELETE FROM contfav WHERE contfav.idContracte = "'.$mostrar['idContracte'].'"';
+            $resultado = mysqli_query($con,$consulta);
+        }
+
+        $consulta = 'DELETE from contracte where contracte.idContracte = "'.$idContracte.'"';
 
         $resultado = mysqli_query($con,$consulta);
-
-        include "mostrar_categoria_favorita.php";
-
     ?>
 </body>
 </html>
