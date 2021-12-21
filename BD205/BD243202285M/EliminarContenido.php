63ee75f65331af3462aa3eea86053986dd03b115
Sr_Tortugo98 <carlosecker.sp@gmail.com>
2021-12-21 01:57:25 +0100

    aa

--- BD205/BD243202285M/EliminarContenido.php
+++ BD205/BD243202285M/EliminarContenido.php
@@ -5,11 +5,13 @@
     <?php
         include "../PHP/conexion.php";
         $vid = $_POST['video'];
-
+        echo '¡Hola ' . htmlspecialchars($_POST["video"]) . '!';
+        echo "ola";
+        echo "$vid";
         $consultaContfav = 'SELECT * FROM contfav WHERE contfav.video = "'.$vid.'"'; //sacamos los contenidos favoritos asociados al contrato
         $contfav = mysqli_query($con, $consultaContfav);
 
-        if(mysqli_num_rows($contfav) > 0){
+        if(!empty($contfav) &&mysqli_num_rows($contfav) > 0){
             while($mostrar=mysqli_fetch_array($contfav)){
                 $eliminarContfav = 'DELETE FROM contfav WHERE contfav.idContracte = "'.$mostrar['idContracte'].'"';
                 $resultado = mysqli_query($con,$eliminarContfav);
@@ -17,7 +19,7 @@
         }
         $missatge = 'SELECT * FROM missatge WHERE missatge.video = "'.$vid.'"'; //sacamos los contenidos favoritos asociados al contrato
         $miss = mysqli_query($con, $missatge);
-        if(mysqli_num_rows($miss) > 0){
+        if(!empty($miss) &&mysqli_num_rows($miss) > 0){
             while($mostrar=mysqli_fetch_array($miss)){
                $eliminarMissatge = 'DELETE FROM missatge WHERE missatge.nomUsuari = "'.$mostrar['nomUsuari'].'"';
                $resultado = mysqli_query($con,$eliminarMissatge);
@@ -27,14 +29,13 @@
         $recomanat = 'SELECT * FROM recomanat WHERE recomanat.video = "'.$vid.'"'; //sacamos los contenidos favoritos asociados al contrato
         $rec = mysqli_query($con, $recomanat);
 
-        if(mysqli_num_rows($rec) > 0){
+        if(!empty($rec) &&mysqli_num_rows($rec) > 0){
             while($mostrar=mysqli_fetch_array($rec)){
                 $eliminarRec = 'DELETE FROM recomanat WHERE recomanat.video = "'.$mostrar['video'].'"';
                 $resultado = mysqli_query($con,$eliminarRec);
             }
         }
 
-      
         $consulta = 'DELETE FROM contingut WHERE contingut.video = "'.$vid.'"';
         $resultado = mysqli_query($con,$consulta);
 
