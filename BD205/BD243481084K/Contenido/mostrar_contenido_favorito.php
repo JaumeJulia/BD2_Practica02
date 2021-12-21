a157e777f696b81c7d7095384079dfc9d8405098
JaumeJulia <Jaumejuli588@gmail.com>
2021-12-21 20:50:28 +0100

    Cualiti imprufment de la administracion de contenido favorito

--- BD205/BD243481084K/Contenido/mostrar_contenido_favorito.php
+++ BD205/BD243481084K/Contenido/mostrar_contenido_favorito.php
@@ -63,7 +63,7 @@ $contenidoFavorito = mysqli_query($con,$consulta);
         <td><b><p style="color:black">Titol</p></b></td> 
         <td><b><p style="color:black">Categoria</p></b></td> 
         <td><b><p style="color:black">Video</p></b></td> 
-        <!-- <td><b><p style="color:black">Añadir a favoritos</p></b></td>  -->
+        <td><b><p style="color:black">Acciones</p></b></td>
     </tr>
 <?php
 
@@ -77,6 +77,10 @@ $contenidoFavorito = mysqli_query($con,$consulta);
                 <td><center><?php echo $reg['titol'] ?></center></td> 
                 <td><center><?php echo $reg['categoria'] ?></center></td> 
                 <td><center><?php echo '<iframe width="560" height="315" src='.getYoutubeEmbedUrl($reg['video']).' title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>' ?></center></td> 
+                <td><form method="post" action="eliminar_contenido_favorito.php">
+                                    <input type="hidden" name="video" value="<?php echo $reg['video'];?>">
+                                    <center><button type="submit">Eliminar</button></center>
+                                    </form></td>
             </tr>
 
         <?php
@@ -85,21 +89,6 @@ $contenidoFavorito = mysqli_query($con,$consulta);
     $contenidoFavorito = mysqli_query($con,$consulta);
     ?>
 </table>
-
-<form action="eliminar_contenido_favorito.php" method="post">
-  <div class="container">
-    <label for="Contenido"><b>Eliminar contenido favorito</b></label>
-    <?php if ($contenidoFavorito != false) { ?>
-        <select name="video" required>
-            <option value=""></option>
-            <?php while ($reg = mysqli_fetch_array($contenidoFavorito)) { //muestra en una lista las categorias no favoritas para poder ser seleccionadas y agregadas?>
-            <option value="<?php echo $reg['video']; ?>"><?php echo $reg['titol'].", url: ".$reg['video']; ?></option>
-            <?php }  ?> 
-        </select>
-    <?php }  ?>    
-    <button type="submit">Eliminar</button>
-  </div>
-</form>
 
 <?php mysqli_close($con); ?>
 
